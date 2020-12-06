e928b79d800efacf08bc398ace40c9441b23fec3
cipharius <cipharius@tase.lv>
2020-12-06 23:37:50 +0200

    Adds desktop notification support

--- home/wm.nix
+++ home/wm.nix
@@ -19,9 +19,47 @@
         playerctl
         light
         xmobar
+        libnotify
       ];
 
       file.".xmonad/xmonad.hs".source = ../dotfiles/xmonad/xmonad.hs;
+    };
+
+    services.dunst = {
+      enable = true;
+      settings = {
+        global = {
+          follow = "keyboard";
+          geometry = "300x8-0+20";
+          indicate_hidden = true;
+          shrink = false;
+          notification_height = 50;
+          separator_height = 1;
+          padding = 8;
+          horizontal_padding = 8;
+          frame_width = 1;
+          frame_color = "#dcdccc";
+          seperator_color = "frame";
+          idle_threshold = 120;
+          font = "DejaVu Sans Mono 8";
+          line_height = 0;
+        };
+        urgency_low = {
+          background = "#707280";
+          foreground = "#707280";
+          timeout = 10;
+        };
+        urgency_normal = {
+          background = "#506070";
+          foreground = "#ffffff";
+          timeout = 10;
+        };
+        urgency_critical = {
+          background = "#cc9393";
+          foreground = "#ffffff";
+          timeout = 0;
+        };
+      };
     };
   };
 }
