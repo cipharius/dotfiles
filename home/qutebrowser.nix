e707fc2eb0bcf7ec1601289b3b20e9c863204642
cipharius <cipharius@tase.lv>
2020-12-06 23:36:20 +0200

    Moves qutebrowser configuration to a file

--- home/qutebrowser.nix
+++ home/qutebrowser.nix
@@ -2,19 +2,10 @@
 
 {
   config = {
-    home.packages = [ pkgs.qutebrowser ];
+    home = {
+      packages = [ pkgs.qutebrowser ];
 
-    home.file.".config/qutebrowser/config.py".text = ''
-      c.aliases = {}
-      c.tabs.tabs_are_windows = True
-      c.tabs.show = "multiple"
-      c.statusbar.show = "in-mode"
-      c.downloads.location.directory = "~/downloads"
-      c.content.pdfjs = True
-      c.editor.command = ["kitty", "kak", "{}"]
-      c.content.javascript.enabled = False
-
-      config.load_autoconfig()
-    '';
+      file.".config/qutebrowser/config.py".source = ../dotfiles/qutebrowser/config.py;
+    };
   };
 }
