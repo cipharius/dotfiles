8f5ed2348c7df197c4d203860f0fa676148d46ad
cipharius <cipharius@tase.lv>
2021-02-04 14:34:13 +0200

    Adds kak-lsp support

--- home/kakoune.nix
+++ home/kakoune.nix
@@ -12,9 +12,13 @@
           ];
         };
       })
+      kak-lsp
+      python-language-server
+      haskellPackages.haskell-language-server
     ];
 
     file.".config/kak/kakrc".source = ../dotfiles/kak/kakrc;
     file.".config/kak/scripts/lf.kak".source = ../dotfiles/kak/scripts/lf.kak;
+    file.".config/kak-lsp/kak-lsp.toml".source = ../dotfiles/kak-lsp/kak-lsp.toml;
   };
 }
